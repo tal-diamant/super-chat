15eda0a9b46ee3a6b375a9cd1ae2ce7a7d30d18a
tal-diamant <erkeliwood@gmail.com>
2022-02-06 10:41:45 +0200

    Done - minus bad words filter and ban

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useRef } from 'react';
 import './App.css';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, query, orderBy, limit, serverTimestamp } from 'firebase/firestore';
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -28,29 +28,71 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <img src="react.png" alt="react icon" />
+          <img src="fire.png" alt="react icon" />
+          <img src="chat.png" alt="react icon" />
+        </div>
+      {user ? <SignOut /> : ''}
       </header>
-        <section>
-          {user ? <ChatRoom /> : <SignIn />}
-        </section>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
+      <footer>
+        <a href="https://www.flaticon.com/free-icons/fire" title="fire icons">Fire icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/react" title="react icons">React icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/conversation" title="conversation icons">Conversation icons created by Freepik - Flaticon</a>
+        <a href="https://www.flaticon.com/free-icons/user" title="user icons">User icons created by Smashicons - Flaticon</a>
+      </footer>
     </div>
   );
 }
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const signInWithGoogle = () => {
       const provider = new GoogleAuthProvider();
       signInWithPopup(auth, provider);
   }
 
+  const signUp = async () => {
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+    } catch(error) {
+      console.log(`code: ${error.code}\nmessage: ${error.message}`);
+    }
+  }
+
+  const signIn = async () => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(auth,email,password);
+    } catch(error) {
+      console.log(`code: ${error.code}\nmessage: ${error.message}`);
+      console.log(error);
+    }
+  }
+
   return (
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      <div className="signup">
+        <label>Email:</label>
+        <input type="email" value={email} onInput={(e)=>setEmail(e.target.value)}/>
+        <label>Password:</label>
+        <input type="password" vlaue={password} onInput={(e)=>setPassword(e.target.value)}/>
+        <button onClick={signUp}>Sign up</button>
+        <button onClick={signIn}>Sign in</button>
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      </div>
   )
 }
 
 const SignOut = () => {
   
   return auth.currentUser && (
-    <button onClick={() => signOut()}>Sign Out</button>
+    <button onClick={() => signOut(auth)}>Sign Out</button>
   )
 }
 
@@ -88,9 +130,10 @@ function ChatRoom() {
     <>
       <main>
         {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        
+        <div ref={dummy}></div>
       </main>
 
-      <div ref={dummy}></div>
 
       <form onSubmit={sendMessage}>
         <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
@@ -100,14 +143,14 @@ function ChatRoom() {
   )
 }
 
-function ChatMessage(props) {
-  const { text, uid, photoURL } = props.message;
+function ChatMessage({message}) {
+  const { text, uid, photoURL } = message;
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recevied';
 
   return (
     <div className={`message ${messageClass}`}>
-      <img src={photoURL} alt="profile picture" />
+      <img src={photoURL? photoURL: "user.png"} alt="profile picture" />
       <p>{text}</p>
     </div>
   )
